83fa52c50ab3de4863bc4446a98a806b2af1cba5
Ron Kirschler <ron.kirschler@gmail.com>
2016-05-06 15:57:37 +0200

    - Added method `deriveFactory` to `View\ViewFactory` to spawen relative Children to a certain ViewFactory

--- src/Factories/CallbackViewFactory.php
+++ src/Factories/CallbackViewFactory.php
@@ -1,25 +1,49 @@
 <?php
 namespace View\Factories;
 
+use View\Helpers\Directories;
+use View\Renderer;
 use View\ViewFactory;
 
 class CallbackViewFactory implements ViewFactory {
 	/** @var callable */
 	private $callback;
+	/** @var null */
+	private $baseDir;
+	/** @var array */
+	private $vars;
 
 	/**
 	 * @param callable $callback
+	 * @param string $baseDir
+	 * @param array $vars
 	 */
-	public function __construct($callback) {
+	public function __construct($callback, $baseDir = null, array $vars = []) {
 		$this->callback = $callback;
+		$this->baseDir = $baseDir;
+		$this->vars = $vars;
 	}
 
 	/**
 	 * @param string $baseDir
 	 * @param array $vars
-	 * @return mixed
+	 * @return Renderer
 	 */
 	public function create($baseDir = null, array $vars = []) {
 		return call_user_func($this->callback, $baseDir, $vars);
+	}
+
+	/**
+	 * @param string $baseDir
+	 * @param array $vars
+	 * @return $this
+	 */
+	public function deriveFactory($baseDir = null, array $vars = []) {
+		if($baseDir === null) {
+			$baseDir = $this->baseDir;
+		} elseif($this->baseDir !== null) {
+			$baseDir = Directories::concat($this->baseDir, $baseDir);
+		}
+		return new static($this->callback, $baseDir, array_merge($this->vars, $vars));
 	}
 }
--- src/ViewFactory.php
+++ src/ViewFactory.php
@@ -8,4 +8,11 @@ interface ViewFactory {
 	 * @return mixed
 	 */
 	public function create($baseDir = null, array $vars = []);
+
+	/**
+	 * @param string $baseDir
+	 * @param array $vars
+	 * @return mixed
+	 */
+	public function deriveFactory($baseDir = null, array $vars = []);
 }
