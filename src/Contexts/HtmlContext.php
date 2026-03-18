62a6d99a688249fac290adcaa47defcf0e8b2582
rkr <ron.kirschler@gmail.com>
2026-03-18 14:15:54 +0100

    Refactor HtmlContext constructor and ensure string typecasting in escape/unescape methods

--- src/Contexts/HtmlContext.php
+++ src/Contexts/HtmlContext.php
@@ -2,22 +2,19 @@
 namespace View\Contexts;
 
 class HtmlContext implements Context {
-	/** @var string */
-	private $charset;
-
 	/**
 	 * @param string $charset
 	 */
-	public function __construct($charset = 'utf-8') {
-		$this->charset = $charset;
-	}
+	public function __construct(
+		private $charset = 'utf-8'
+	) {}
 
 	/**
 	 * @param string $value
 	 * @return string
 	 */
 	public function escape($value) {
-		return htmlentities($value, ENT_COMPAT, $this->charset);
+		return htmlentities((string) $value, ENT_COMPAT, $this->charset);
 	}
 
 	/**
@@ -25,6 +22,6 @@ class HtmlContext implements Context {
 	 * @return string
 	 */
 	public function unescape($value) {
-		return html_entity_decode($value, ENT_COMPAT, $this->charset);
+		return html_entity_decode((string) $value, ENT_COMPAT, $this->charset);
 	}
 }
